5b24e73db0217c0a0dc768be1c63a0962d987330
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-10-31 00:52:15 +0530

    Update machine.cpp

--- machine.cpp
+++ machine.cpp
@@ -6,7 +6,7 @@
 const int motorDriverPin1 = 32;
 const int motorDriverPin2 = 33;
 const int pressureValvePin = 18;
-const int switchPin = 25;
+const int switchPin = 19;
 
 // Define the pressure sensor calibration parameters
 const float pressureSensorOffset = 100.0;
@@ -18,13 +18,27 @@ const int korotkoffCountThreshold = 5; // The number of Korotkoff sounds to dete
 
 // Define the pump states
 const int OFF = 0;
-const int ON = 1;
+const int INFLATING = 1;
+const int MEASURING = 2;
+const int DEFLATING = 3;
 
 // Create an instance of the Adafruit_MPU6050 class
 Adafruit_MPU6050 mpu;
 
 // Define the pins for the pressure sensor
 const int pressureSensorOutputPin = A0; // Assuming the pressure sensor is connected to analog pin A0
+
+// Define variables for pump and valve control
+int pumpState = OFF;
+int valveState = LOW; // Assume the valve is initially closed
+unsigned long inflatingStartTime = 0;
+unsigned long measuringStartTime = 0;
+
+// Variables for pressure measurement
+int systolicPressure = 0;
+int diastolicPressure = 0;
+int korotkoffCount = 0;
+int previousPressure = 0;
 
 void setup() {
   // Initialize the serial port
@@ -39,6 +53,7 @@ void setup() {
 
   // Set the pressure valve pin to output mode
   pinMode(pressureValvePin, OUTPUT);
+  digitalWrite(pressureValvePin, valveState); // Set valve to initial state (closed)
 
   // Set the switch pin to input mode
   pinMode(switchPin, INPUT);
@@ -49,46 +64,66 @@ void loop() {
   int switchState = digitalRead(switchPin);
 
   if (switchState == HIGH) {
-    // If the switch is ON, turn on the pump
-    pumpState = ON;
+    // If the switch is ON, start the blood pressure measurement cycle
+    pumpState = INFLATING;
+    inflatingStartTime = millis(); // Record the start time of inflation
     digitalWrite(motorDriverPin1, HIGH);
     digitalWrite(motorDriverPin2, LOW);
-  } else {
-    // If the switch is OFF, turn off the pump
-    pumpState = OFF;
-    digitalWrite(motorDriverPin1, LOW);
-    digitalWrite(motorDriverPin2, HIGH);
+    valveState = HIGH; // Open the valve
   }
 
-  // Read the pressure sensor data
-  int pressure = analogRead(pressureSensorOutputPin);
-  float pressureValue = pressure * pressureSensorScaleFactor + pressureSensorOffset;
-
-  // Calculate the systolic and diastolic blood pressure using the Korotkoff algorithm
-  int systolicPressure = 0;
-  int diastolicPressure = 0;
-  int korotkoffCount = 0;
-  int previousPressure = pressure;
-
-  // Detect the Korotkoff sounds
-  while (korotkoffCount < korotkoffCountThreshold) {
-    if (abs(pressure - previousPressure) > korotkoffThreshold) {
-      korotkoffCount++;
-      if (korotkoffCount == 1) {
-        systolicPressure = pressureValue;
+  // Implement different states of the blood pressure cycle
+  switch (pumpState) {
+    case INFLATING:
+      // Control the pump to inflate the cuff (you may need to set a time or pressure threshold)
+      if (millis() - inflatingStartTime > 5000) { // Inflate for 5 seconds as an example
+        pumpState = MEASURING;
+        measuringStartTime = millis(); // Record the start time of measurement
       }
-    }
+      break;
 
-    previousPressure = pressure;
-    pressure = analogRead(pressureSensorOutputPin);
-    pressureValue = pressure * pressureSensorScaleFactor + pressureSensorOffset;
+    case MEASURING:
+      // Continue measuring for a specific duration (e.g., 10 seconds)
+      if (millis() - measuringStartTime > 10000) { // Measure for 10 seconds as an example
+        pumpState = DEFLATING;
+        valveState = LOW; // Close the valve to release pressure
+      } else {
+        // During the measurement phase, read the pressure sensor data
+        int pressure = analogRead(pressureSensorOutputPin);
+        // Calculate systolic and diastolic pressure using the Korotkoff algorithm
+        if (abs(pressure - previousPressure) > korotkoffThreshold) {
+          korotkoffCount++;
+          if (korotkoffCount == 1) {
+            systolicPressure = pressure;
+          }
+          if (korotkoffCount == korotkoffCountThreshold) {
+            diastolicPressure = pressure;
+          }
+        }
+        previousPressure = pressure;
+      }
+      break;
+
+    case DEFLATING:
+      // Control the pressure valve to release the pressure
+      digitalWrite(pressureValvePin, valveState); // Open or close the valve based on valveState
+      // The pressure valve is controlled here to release pressure
+      // You may need to set a time duration for deflation or control it based on your mechanism
+      // Consult your hardware documentation for the control mechanism
+      break;
+
+    default:
+      // Pump is OFF, do nothing
+      break;
   }
 
-  diastolicPressure = pressureValue;
+  // Display systolic and diastolic pressure readings
+  if (pumpState == DEFLATING) {
+    Serial.print("Systolic pressure: ");
+    Serial.println(systolicPressure);
+    Serial.print("Diastolic pressure: ");
+    Serial.println(diastolicPressure);
+  }
 
-  // Display the systolic and diastolic blood pressure readings on the serial port
-  Serial.print("Systolic pressure: ");
-  Serial.println(systolicPressure);
-  Serial.print("Diastolic pressure: ");
-  Serial.println(diastolicPressure);
+  // Add any additional code here if needed
 }
