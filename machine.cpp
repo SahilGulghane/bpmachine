fbade845d84351446725ac621499c0ab58809187
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-10-31 02:02:24 +0530

    Update machine.cpp

--- machine.cpp
+++ machine.cpp
@@ -1,104 +1,73 @@
-#include <Q2HX711.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
+#include <Arduino.h>
 
-// Define the pins that the pressure sensor, motor driver, pressure valve, and switch are connected to
+// Define the pins for the switch, air pump, pressure valve, and pressure sensor
+const int switchPin = 25;
+const int airPumpPin1 = 33;
+const int airPumpPin2 = 32;
+const int pressureValvePin = 18;
 const int pressureSensorClockPin = 35;
 const int pressureSensorOutputPin = 34;
-const int motorDriverPin1 = 32;
-const int motorDriverPin2 = 33;
-const int pressureValvePin = 18;
-const int switchPin = 19;
 
-// Define the pressure sensor calibration parameters
-const float pressureSensorOffset = 100.0;
-const float pressureSensorScaleFactor = 1.0;
+// Define the calibration constants for the pressure sensor
+const float pressureSensorCalibrationOffset = 0.0f;
+const float pressureSensorCalibrationFactor = 1.0f;
 
-// Define the Korotkoff algorithm parameters
-const int korotkoffThreshold = 5; // The minimum difference in pressure sensor readings to be considered a Korotkoff sound
-const int korotkoffCountMax = 5; // The maximum number of Korotkoff sounds to detect
-
-// Create an instance of the Q2HX711 class
-Q2HX711 MPS20N0040D(pressureSensorOutputPin, pressureSensorClockPin);
-
-// Define the pressure valve state
-const int pressureValveOpen = HIGH;
-const int pressureValveClosed = LOW;
-
+// Initialize the air pump and pressure sensor
 void setup() {
-  // Initialize the serial port
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // Initialize the Q2HX711 class
-  MPS20N0040D.begin();
-
-  // Set the motor driver pins to output mode
-  pinMode(motorDriverPin1, OUTPUT);
-  pinMode(motorDriverPin2, OUTPUT);
-
-  // Set the pressure valve pin to output mode
-  pinMode(pressureValvePin, OUTPUT);
-
-  // Set the switch pin to input mode
   pinMode(switchPin, INPUT);
+  pinMode(airPumpPin1, OUTPUT);
+  pinMode(airPumpPin2, OUTPUT);
+  pinMode(pressureValvePin, OUTPUT);
+  pinMode(pressureSensorClockPin, INPUT);
+  pinMode(pressureSensorOutputPin, INPUT);
 }
 
+// Main loop
 void loop() {
-  // Read the switch state
-  int switchState = digitalRead(switchPin);
+  // Check if the switch is turned on
+  if (digitalRead(switchPin) == HIGH) {
+    // Turn on the air pump
+    digitalWrite(airPumpPin1, HIGH);
+    digitalWrite(airPumpPin2, HIGH);
 
-  if (switchState == HIGH) {
-    // Turn on the pump and open the pressure valve
-    digitalWrite(motorDriverPin1, HIGH);
-    digitalWrite(motorDriverPin2, LOW);
-    digitalWrite(pressureValvePin, pressureValveOpen);
+    // Inflate the cuff to a certain pressure
+    delay(10000); // Inflate the cuff for 10 seconds
 
-    // Measure blood pressure
-    measureBloodPressure();
-  } else {
-    // Turn off the pump and close the pressure valve
-    digitalWrite(motorDriverPin1, LOW);
-    digitalWrite(motorDriverPin2, HIGH);
-    digitalWrite(pressureValvePin, pressureValveClosed);
+    // Read the pressure sensor to get the systolic pressure
+    int systolicPressure = readPressureSensor();
 
-    // Add a delay to avoid rapid changes
-    delay(1000);
+    // Open the pressure valve to deflate the cuff
+    digitalWrite(pressureValvePin, HIGH);
+
+    // Deflate the cuff
+    delay(5000); // Deflate the cuff for 5 seconds
+
+    // Read the pressure sensor again to get the diastolic pressure
+    int diastolicPressure = readPressureSensor();
+
+    // Display the systolic and diastolic pressures on the LCD display
+    Serial.println("Systolic pressure: " + String(systolicPressure));
+    Serial.println("Diastolic pressure: " + String(diastolicPressure));
+
+    // Turn off the air pump
+    digitalWrite(airPumpPin1, LOW);
+    digitalWrite(airPumpPin2, LOW);
+
+    // Close the pressure valve
+    digitalWrite(pressureValvePin, LOW);
   }
 }
 
-void measureBloodPressure() {
-  // Read the pressure sensor data
-  float pressure = MPS20N0040D.read();
+// Reads the pressure sensor and returns the pressure in mmHg
+int readPressureSensor() {
+  // Read the pressure sensor output pin
+  int pressureSensorOutput = analogRead(pressureSensorOutputPin);
 
-  // Check for a valid pressure reading
-  if (pressure >= 0.0 && pressure <= 1000.0) {
-    // Calculate systolic and diastolic blood pressure using the Korotkoff algorithm
-    int systolicPressure = 0;
-    int diastolicPressure = 0;
-    int korotkoffCount = 0;
-    float previousPressure = 0.0;
+  // Convert the pressure sensor output to a pressure in mmHg
+  float pressure = (pressureSensorOutput - pressureSensorCalibrationOffset) * pressureSensorCalibrationFactor;
 
-    while (pressure > korotkoffThreshold && korotkoffCount < korotkoffCountMax) {
-      if (pressure < systolicPressure) {
-        systolicPressure = pressure;
-      }
-
-      if (pressure - previousPressure > korotkoffThreshold) {
-        korotkoffCount++;
-      }
-
-      previousPressure = pressure;
-
-      // Read the pressure sensor data
-      pressure = MPS20N0040D.read();
-    }
-
-    // Display the systolic and diastolic blood pressure readings on the serial port
-    Serial.print("Systolic pressure: ");
-    Serial.println(systolicPressure);
-    Serial.print("Diastolic pressure: ");
-    Serial.println(diastolicPressure);
-  }
+  // Return the pressure
+  return pressure;
 }
